5a23d3a3fd87aec099f7a14d6e31ccf109cfcb02
Paul T <developer.paul.123@gmail.com>
2023-07-28 09:39:11 -0400

    Increase "large" resolution

--- Chess-Challenge/src/Framework/Application/Core/Settings.cs
+++ Chess-Challenge/src/Framework/Application/Core/Settings.cs
@@ -14,8 +14,8 @@ namespace ChessChallenge.Application
         // Display settings
         public const bool DisplayBoardCoordinates = true;
         public static readonly Vector2 ScreenSizeSmall = new(1280, 720);
-        public static readonly Vector2 ScreenSizeBig = new(1920, 1080);
-
+        public static readonly Vector2 ScreenSizeBig = new(2160, 1440);
+        
         // Other settings
         public const int MaxTokenCount = 1024;
         public const LogType MessagesToLog = LogType.All;
