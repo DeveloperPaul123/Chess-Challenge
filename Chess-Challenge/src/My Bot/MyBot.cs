39fa89cb902bf33c2706e65226e085477891ede2
Paul T <developer.paul.123@gmail.com>
2023-08-02 09:32:53 -0400

    Reduce token count in my bot for final submission
    Misc. code cleanup and some minor renaming

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,19 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Linq;
 using ChessChallenge.API;
 
-public abstract class IBoardEvaluator
+public class MyBot : IChessBot
 {
-    public abstract int Evaluate(Board board);
-}
-
-internal class PestoEvaluation : IBoardEvaluator
-{
-    // PEsTo eval
+    // PeSTO evaluation
     // https://www.chessprogramming.org/PeSTO%27s_Evaluation_Function
-    private readonly int[] _pieceValues = { 100, 310, 330, 500, 1000, 10000 };
-    private readonly int[] _piecePhase = { 0, 1, 1, 2, 4, 0 };
+    private readonly int[] _pieceValues =
+    {
+        0, 82, 337, 365, 477, 1025, 20000,
+        0, 94, 281, 297, 512, 936, 20000
+    };
 
+    private readonly int[] _piecePhase = { 0, 0, 1, 1, 2, 4, 0 };
+
+    // This is all the pesto value tables compressed into a single array
     private static readonly ulong[] CompressedPestoValues =
     {
         657614902731556116, 420894446315227099, 384592972471695068, 312245244820264086, 364876803783607569,
@@ -34,130 +35,6 @@ internal class PestoEvaluation : IBoardEvaluator
         329978099633296596, 67159620133902
     };
 
-    private int GetPestoValue(int index)
-    {
-        return (int)(((CompressedPestoValues[index / 10] >> (6 * (index % 10))) & 63) - 20) * 8;
-    }
-
-    public override int Evaluate(Board board)
-    {
-        var mg = 0;
-        var eg = 0;
-        var phase = 0;
-
-        for (sbyte b = 0; b <= 1; b++)
-        {
-            // evaluate white first
-            var isWhite = b == 0;
-            for (var piece = PieceType.Pawn; piece <= PieceType.King; piece++)
-            {
-                var mask = board.GetPieceBitboard(piece, isWhite);
-                while (mask != 0)
-                {
-                    phase += _piecePhase[(int)piece - 1];
-                    var index = 128 * ((int)piece - 1) + BitboardHelper.ClearAndGetIndexOfLSB(ref mask) ^
-                                (isWhite ? 56 : 0);
-                    mg += GetPestoValue(index) + _pieceValues[(int)piece - 1];
-                    eg += GetPestoValue(index + 64) + _pieceValues[(int)piece - 1];
-                }
-            }
-
-            mg = -mg;
-            eg = -eg;
-        }
-
-        return (mg * phase + eg * (24 - phase)) / 24;
-        // return (mg * phase + eg * (24 - phase)) / 24 * (board.IsWhiteToMove ? 1 : -1);
-    }
-}
-
-internal class SimpleEvaluation : IBoardEvaluator
-{
-    // Piece values (material values) - followed the order of the PieceType enum
-    private readonly int[] _pieceValues = { 0, 100, 300, 320, 500, 900, 20000 };
-
-    // Piece square tables (these tables indicate the positional strength of each pieces
-    // This is a packed evaluation table of tables (each ulong is a table for a piece type)
-    private readonly ulong[,] _packedEvaluationTables =
-    {
-        { 58233348458073600, 61037146059233280, 63851895826342400, 66655671952007680 },
-        { 63862891026503730, 66665589183147058, 69480338950193202, 226499563094066 },
-        { 63862895153701386, 69480338782421002, 5867015520979476, 8670770172137246 },
-        { 63862916628537861, 69480338782749957, 8681765288087306, 11485519939245081 },
-        { 63872833708024320, 69491333898698752, 8692760404692736, 11496515055522836 },
-        { 63884885386256901, 69502350490469883, 5889005753862902, 8703755520970496 },
-        { 63636395758376965, 63635334969551882, 21474836490, 1516 },
-        { 58006849062751744, 63647386663573504, 63625396431020544, 63614422789579264 }
-    };
-
-    private int GetPieceSquareValue(Piece piece)
-    {
-        var file = piece.Square.File;
-        var rank = piece.Square.Rank;
-
-        if (file > 3)
-            file = 7 - file;
-
-        // mirror for black pieces
-        if (piece.IsWhite)
-            rank = 7 - rank;
-
-        var unpackedData =
-            unchecked((sbyte)((_packedEvaluationTables[rank, file] >> 8 * ((int)piece.PieceType - 1)) & 0xFF));
-        return piece.IsWhite ? unpackedData : -unpackedData;
-    }
-
-    private int PieceMobility(Piece piece, Board board)
-    {
-        var bitBoard = board.GetPieceBitboard(piece.PieceType, piece.IsWhite);
-        var count = 0;
-        while (bitBoard != 0)
-        {
-            count++;
-            bitBoard &= bitBoard - 1; // reset LS1B
-        }
-
-        return count;
-    }
-
-    public override int Evaluate(Board board)
-    {
-        if (board.IsRepeatedPosition()) return 0;
-
-        var score = 0;
-
-        for (var pieceType = PieceType.Pawn; pieceType <= PieceType.King; pieceType++)
-        {
-            var whitePieces = board.GetPieceList(pieceType, true);
-            var blackPieces = board.GetPieceList(pieceType, false);
-
-            foreach (var piece in whitePieces)
-            {
-                score += _pieceValues[(int)pieceType];
-                score += GetPieceSquareValue(piece);
-                score += PieceMobility(piece, board);
-            }
-
-            foreach (var piece in blackPieces)
-            {
-                score -= _pieceValues[(int)pieceType];
-                score -= GetPieceSquareValue(piece);
-                score -= PieceMobility(piece, board);
-            }
-        }
-
-        // TODO: Take into account piece structure
-        // TODO: Take into account king safety
-        // TODO: Take into account piece board control
-
-        return score;
-    }
-}
-
-public class MyBot : IChessBot
-{
-    private readonly IBoardEvaluator _chessEvaluator;
-
     // used for move ordering: https://www.chessprogramming.org/MVV-LVA
     // most valuable victim - least valuable attacker note that the order is based on the enumeration
     // order for PieceType as it is used as an index
@@ -167,14 +44,17 @@ public class MyBot : IChessBot
 
     // lva -> pawn, knight, bishop, rook, queen, king
     private readonly int[] _lvaValues = { 0, 5, 4, 3, 2, 1, 0 };
+
+    // match types for transposition table
     private const sbyte Exact = 0, LowerBound = -1, UpperBound = 1, Invalid = -2;
+
 #if DEBUG
     private const int MaxDepth = 3;
 #else
     private const int MaxDepth = 6;
 #endif
 
-//14 bytes per entry, likely will align to 16 bytes due to padding (if it aligns to 32, recalculate max TP table size)
+    //14 bytes per entry, likely will align to 16 bytes due to padding (if it aligns to 32, recalculate max TP table size)
     private struct Transposition
     {
         public ulong ZobristHash = 0;
@@ -188,30 +68,41 @@ public class MyBot : IChessBot
         }
     };
 
-    private const ulong TranspositionTableMask = 0x7FFFFF; //4.7 million entries, likely consuming about 151 MB
+    // 4.7 million entries, likely consuming about 151 MB
+    private const ulong TranspositionTableMask = 0x7FFFFF;
     private readonly Transposition[] _transpositionTable = new Transposition[TranspositionTableMask + 1];
 
-    public MyBot(IBoardEvaluator chessEvaluator)
+    // maximum think time for each move 
+    // private const int MaxThinkTime = 10 * 1000;
+
+    /// <summary>
+    /// Negamax search with alpha-beta pruning and transposition table
+    /// </summary>
+    /// <param name="board"></param>
+    /// <param name="depth"></param>
+    /// <param name="alpha"></param>
+    /// <param name="beta"></param>
+    /// <returns></returns>
+    private int Search(Board board, int depth, int alpha, int beta)
     {
-        _chessEvaluator = chessEvaluator;
-    }
+        if (depth <= 0) return Quiesce(board, alpha, beta);
 
-    private const int MaxThinkTime = 10 * 1000; //5 seconds
-
-    private int Negamax(Board board, int depth, int alpha, int beta)
-    {
-        if (depth <= 0)
-        {
-            return Quiesce(board, alpha, beta);
-        }
-
-        ref Transposition transposition = ref _transpositionTable[board.ZobristKey & TranspositionTableMask];
+        ref var transposition = ref _transpositionTable[board.ZobristKey & TranspositionTableMask];
         if (transposition.ZobristHash == board.ZobristKey && transposition.Flag != Invalid &&
             transposition.Depth >= depth)
         {
-            if (transposition.Flag == Exact) return transposition.Evaluation;
-            else if (transposition.Flag == LowerBound) alpha = Math.Max(alpha, transposition.Evaluation);
-            else if (transposition.Flag == UpperBound) beta = Math.Min(beta, transposition.Evaluation);
+            switch (transposition.Flag)
+            {
+                case Exact:
+                    return transposition.Evaluation;
+                case LowerBound:
+                    alpha = Math.Max(alpha, transposition.Evaluation);
+                    break;
+                case UpperBound:
+                    beta = Math.Min(beta, transposition.Evaluation);
+                    break;
+            }
+
             if (alpha >= beta) return transposition.Evaluation;
         }
 
@@ -221,7 +112,7 @@ public class MyBot : IChessBot
         // really hate to lose
         if (board.IsInCheckmate()) return int.MinValue + board.PlyCount;
 
-        int startingAlpha = alpha;
+        var startingAlpha = alpha;
         var moves = board.GetLegalMoves();
         OrderMoves(ref moves, board);
 
@@ -230,7 +121,7 @@ public class MyBot : IChessBot
         foreach (var move in moves)
         {
             board.MakeMove(move);
-            var eval = -Negamax(board, depth - 1, -beta, -alpha);
+            var eval = -Search(board, depth - 1, -beta, -alpha);
             board.UndoMove(move);
             if (bestScore < eval)
             {
@@ -239,10 +130,7 @@ public class MyBot : IChessBot
             }
 
             alpha = Math.Max(alpha, bestScore);
-            if (alpha >= beta)
-            {
-                break;
-            }
+            if (alpha >= beta) break;
         }
 
         transposition.Evaluation = bestScore;
@@ -258,8 +146,8 @@ public class MyBot : IChessBot
 
     private int GetMovePriority(Move move, Board board)
     {
-        int priority = 0;
-        Transposition tp = _transpositionTable[board.ZobristKey & TranspositionTableMask];
+        var priority = 0;
+        var tp = _transpositionTable[board.ZobristKey & TranspositionTableMask];
         if (tp.Move == move && tp.ZobristHash == board.ZobristKey) priority += 1000;
         if (move.IsCapture) priority = _mvvValues[(int)move.CapturePieceType] + _lvaValues[(int)move.MovePieceType];
         return priority;
@@ -267,10 +155,9 @@ public class MyBot : IChessBot
 
     private void OrderMoves(ref Move[] moves, Board board)
     {
-        List<Tuple<Move, int>> orderedMoves = new();
-        foreach (Move m in moves) orderedMoves.Add(new Tuple<Move, int>(m, GetMovePriority(m, board)));
+        var orderedMoves = moves.Select(m => new Tuple<Move, int>(m, GetMovePriority(m, board))).ToList();
         orderedMoves.Sort((a, b) => b.Item2.CompareTo(a.Item2));
-        for (int i = 0; i < moves.Length; i++) moves[i] = orderedMoves[i].Item1;
+        for (var i = 0; i < moves.Length; i++) moves[i] = orderedMoves[i].Item1;
     }
 
     private int Quiesce(Board board, int alpha, int beta)
@@ -289,10 +176,10 @@ public class MyBot : IChessBot
 
         OrderMoves(ref moves, board);
 
-        foreach (Move m in moves)
+        foreach (var m in moves)
         {
             board.MakeMove(m);
-            int evaluation = -Quiesce(board, -beta, -alpha);
+            var evaluation = -Quiesce(board, -beta, -alpha);
             board.UndoMove(m);
 
             alpha = Math.Max(evaluation, alpha);
@@ -302,16 +189,44 @@ public class MyBot : IChessBot
         return alpha;
     }
 
-    private static bool ShouldExecuteNextDepth(Timer timer, int maxThinkTime)
+    // private static bool ShouldExecuteNextDepth(Timer timer, int maxThinkTime)
+    // {
+    //     var currentThinkTime = timer.MillisecondsElapsedThisTurn;
+    //     return ((maxThinkTime - currentThinkTime) > currentThinkTime * 3);
+    // }
+
+    private int GetPestoValue(int index) => (int)(((CompressedPestoValues[index / 10] >> (6 * (index % 10))) & 63) - 20) * 8;
+
+    public int Evaluate(Board board)
     {
-        var currentThinkTime = timer.MillisecondsElapsedThisTurn;
-        return ((maxThinkTime - currentThinkTime) > currentThinkTime * 3);
+        int mg = 0, eg = 0, phase = 0;
+
+        for (sbyte b = 0; b <= 1; b++)
+        {
+            // evaluate white first
+            var isWhite = b == 0;
+            for (var piece = PieceType.Pawn; piece <= PieceType.King; piece++)
+            {
+                var mask = board.GetPieceBitboard(piece, isWhite);
+                while (mask != 0)
+                {
+                    phase += _piecePhase[(int)piece];
+                    var index = 128 * ((int)piece - 1) + BitboardHelper.ClearAndGetIndexOfLSB(ref mask) ^
+                                (isWhite ? 56 : 0);
+                    // subtract 1 when getting pesto value because pesto values are 1-indexed
+                    mg += GetPestoValue(index) + _pieceValues[(int)piece];
+                    eg += GetPestoValue(index + 64) + _pieceValues[(int)piece + 6];
+                }
+            }
+
+            mg = -mg;
+            eg = -eg;
+        }
+
+        return ((mg * phase + eg * (24 - phase)) / 24) * (board.IsWhiteToMove ? 1 : -1);
     }
 
-    private int Evaluate(Board board)
-    {
-        return _chessEvaluator.Evaluate(board) * (board.IsWhiteToMove ? 1 : -1);
-    }
+    // private int Evaluate(Board board) => _chessEvaluator.Evaluate(board) * (board.IsWhiteToMove ? 1 : -1);
 
     public Move Think(Board board, Timer timer)
     {
@@ -321,10 +236,13 @@ public class MyBot : IChessBot
              depth <= MaxDepth;
              depth++)
         {
-            Negamax(board, MaxDepth, int.MinValue + 1, int.MaxValue - 1);
+            Search(board, MaxDepth, int.MinValue + 1, int.MaxValue - 1);
             bestMove = _transpositionTable[board.ZobristKey & TranspositionTableMask];
 
-            if (!ShouldExecuteNextDepth(timer, MaxThinkTime)) break;
+            var currentThinkTime = timer.MillisecondsElapsedThisTurn;
+            var shouldExecuteNextDepth = ((10 * 1000) - currentThinkTime) > (currentThinkTime * 3);
+
+            if (!shouldExecuteNextDepth) break;
         }
 
         return bestMove.Move;
