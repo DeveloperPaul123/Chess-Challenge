7256c741b9b4688853b645e6f0b7bcef76c539d8
Paul T <developer.paul.123@gmail.com>
2023-08-07 07:34:34 -0400

    Add script to compare engines
    Also added book data

---
+++ Scripts/CompareEngines.ps1
@@ -0,0 +1,15 @@
+D:\Repositories\cutechess\build-qt6\Release\cutechess-cli.exe `
+    -engine name="MyBot" cmd="$PSScriptRoot/../Chess-Challenge/bin/Release/net6.0/Chess-Challenge.exe" arg="uci" arg="MyBot" `
+    -engine name="MyBotv5" cmd="$PSScriptRoot/../Chess-Challenge/bin/Release/net6.0/Chess-Challenge.exe" arg="uci" arg="MyBotv5" `
+    -each proto=uci tc=0/5+0 `
+    -maxmoves 2000 `
+    -games 2 `
+    -repeat `
+    -resultformat wide2 `
+    -ratinginterval 20 `
+    -rounds 500 `
+    -concurrency 10 `
+    # -tournament gauntlet `
+    -pgnout out.pgn `
+    -sprt elo0=0 elo1=5 alpha=0.05 beta=0.05 `
+    -openings file="$PSScriptRoot/UHO_XXL_+0.80_+1.09.epd" format=epd
