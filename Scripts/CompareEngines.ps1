14e3c7ba8abe51b9a2643ae82557088299b104a9
Paul T <developer.paul.123@gmail.com>
2023-08-09 17:23:17 -0400

    Fix default script for comparing engines

--- Scripts/CompareEngines.ps1
+++ Scripts/CompareEngines.ps1
@@ -1,14 +1,14 @@
 D:\Repositories\cutechess\build-qt6\Release\cutechess-cli.exe `
     -engine name="MyBot" cmd="$PSScriptRoot/../Chess-Challenge/bin/Release/net6.0/Chess-Challenge.exe" arg="uci" arg="MyBot" `
-    -engine name="Tier2" cmd="$PSScriptRoot/../Chess-Challenge/bin/Release/net6.0/Chess-Challenge.exe" arg="uci" arg="NegamaxTier2Bot" `
-    -each proto=uci tc=0/5+0 `
+    -engine name="MyBotv5" cmd="$PSScriptRoot/../Chess-Challenge/bin/Release/net6.0/Chess-Challenge.exe" arg="uci" arg="MyBotv5" `
+    -each proto=uci tc=0/10+0 `
     -maxmoves 2000 `
     -games 2 `
     -repeat `
     -resultformat wide2 `
     -ratinginterval 20 `
     -rounds 500 `
-    -concurrency 10 `
+    -concurrency 8 `
     -sprt elo0=0 elo1=5 alpha=0.05 beta=0.05 `
     -pgnout "$PSScriptRoot/out.pgn" `
-    # -openings file="$PSScriptRoot/UHO_XXL_+0.80_+1.09.epd" format=epd
+    -openings file="$PSScriptRoot/UHO_XXL_+1.00_+1.29.pgn" order=random
