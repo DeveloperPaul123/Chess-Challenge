46f15049d29523d1794a369d836c4a4f6a93da5c
Paul T <developer.paul.123@gmail.com>
2023-08-14 13:41:56 -0400

    Minor unit test tweaks

--- Chess-Challenge.Tests/MyBotTests.cs
+++ Chess-Challenge.Tests/MyBotTests.cs
@@ -8,12 +8,13 @@ namespace Chess_Challenge.Tests
     [TestClass]
     public class MyBotTests
     {
-        private readonly MyBot _myBot = new MyBot();
-        private List<KeyValuePair<string, string>> testPositions;
+        private readonly MyBot _myBot = new();
+        private List<KeyValuePair<string, string>> _testPositions;
+
         [TestInitialize]
         public void Initialize()
         {
-            testPositions = new List<KeyValuePair<string, string>>
+            _testPositions = new List<KeyValuePair<string, string>>
             {
                 new("r1bqkb1r/ppp2pp1/5n1p/3P4/2P1p3/5N2/PPP2PPP/RNBQK2R w KQkq","f3d4"),
                 new("r1bqk2r/pp1n1ppp/4pn2/2b5/3P4/3B1N2/PPP2PPP/R1BQ1RK1 w kq", "d4c5")
@@ -24,7 +25,7 @@ namespace Chess_Challenge.Tests
         public void TestExpectedPositions()
         {
             var boardSource = new ChessChallenge.Chess.Board();
-            foreach (var testPosition in testPositions)
+            foreach (var testPosition in _testPositions)
             {
                 boardSource.LoadPosition(FenUtility.PositionFromFen(testPosition.Key));
                 var board = new Board(boardSource);
