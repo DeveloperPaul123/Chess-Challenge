041012f0c35e54f94c72f35eff6ca68be532ef73
Paul T <developer.paul.123@gmail.com>
2023-09-12 16:57:36 -0400

    Update base comparison script

--- Scripts/CompareEngines.ps1
+++ Scripts/CompareEngines.ps1
@@ -1,13 +1,13 @@
 D:\Repositories\cutechess\build-qt6\Release\cutechess-cli.exe `
     -engine name="MyBot" cmd="$PSScriptRoot/../Chess-Challenge/bin/Release/net6.0/Chess-Challenge.exe" arg="uci" arg="MyBot" `
-    -engine name="MyBotv5" cmd="$PSScriptRoot/../Chess-Challenge/bin/Release/net6.0/Chess-Challenge.exe" arg="uci" arg="MyBotv5" `
-    -each proto=uci tc=0/30+0 `
+    -engine name="MyBotv621" cmd="$PSScriptRoot/../Chess-Challenge/bin/Release/net6.0/Chess-Challenge.exe" arg="uci" arg="MyBotv621" `
+    -each proto=uci tc=0/10+0 `
     -maxmoves 2000 `
     -games 2 `
     -repeat `
     -resultformat wide2 `
     -ratinginterval 20 `
-    -rounds 300 `
+    -rounds 5000 `
     -concurrency 6 `
     -sprt elo0=0 elo1=5 alpha=0.05 beta=0.05 `
     -pgnout "$PSScriptRoot/out.pgn" `
